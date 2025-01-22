40b45f6b525da08591bc95751be270e8a5d27211
Jack Carney <jackcarney09@outlook.com>
2025-01-22 14:53:20 +0000

    class views pt2

--- Noclue/urls.py
+++ Noclue/urls.py
@@ -4,6 +4,8 @@ from .import views
 urlpatterns = [
     path('', views.LogInFormView.as_view(), name='index'),
     path ('test', views.ThankYouView.as_view(), name='test'),
-    path('users', views.UserInfoView.as_view(), name='users')
+    path('users', views.UserInfoView.as_view(), name='users'),
+    path('users/<int:pk>', views.UserDetailView.as_view(), name='user_info'),
+    
 ]
 
--- Noclue/views.py
+++ Noclue/views.py
@@ -1,42 +1,50 @@
-from django.shortcuts import render 
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404 
+from django.http import HttpResponseRedirect, Http404
 from .forms import LogInForm
 from .models import LogIn
 from django.views import View
 from django.contrib.auth import login
-from django.views.generic import TemplateView
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView, CreateView
+
+#This is the view for the login form using the FormView
+# class LogInFormView(FormView):
+#    form_class = LogInForm
+#    template_name = 'Noclue/index.html'
+#    success_url = '/test'
+
+#    def form_valid(self, form):
+#        form.save()
+#        return super().form_valid(form)
 
 
-class LogInFormView(View):
-    def get(self, request):
-        form = LogInForm()
-        return render(request, 'Noclue/index.html', {
-            'form': form
-        })
 
-    def post(self, request):
-        form = LogInForm(request.POST)
-        
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/test')
-
-        return render(request, 'Noclue/index.html', {
-            'form': form
-        })
+#This is a view for the login form using the CreateView
+class LogInFormView(CreateView):
+    model = LogIn
+    form_class = LogInForm
+    template_name = 'Noclue/index.html'
+    success_url = '/test'
 
 
 class ThankYouView(TemplateView):
     template_name = 'Noclue/test.html'
     
+    
 
-class UserInfoView(TemplateView):
+class UserInfoView(ListView):
     template_name = 'Noclue/Users.html'
     
-    def get_context_data(self, **kwargs):
-            context = super().get_context_data(**kwargs)
-            user = LogIn.objects.all()
-            context['user'] = user
-            return context
-        
-        
+    model = LogIn
+    context_object_name = 'user'
+
+
+
+class UserDetailView(DetailView):
+    template_name = 'Noclue/user_info.html'
+    model = LogIn
+    
+    context_object_name = 'user_details'
+    
+    
